e6622741c3d0a383cf9050affe2a95f04c75acee
tushverma <contacttusharverma@gmail.com>
2021-08-23 16:08:46 +0530

    add superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -18,12 +18,12 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """Create and save a new super user"""
         user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
-        user.save(using=self.__db)
+        user.save(using=self._db)
         return user
 
 
